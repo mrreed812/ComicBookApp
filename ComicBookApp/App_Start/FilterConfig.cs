d57aedb7daf97107696e6fea11632fdf809da102
marlener <mrreed812@gmail.com>
2018-11-06 18:20:21 -0500

    test

---
+++ ComicBookApp/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace ComicBookApp
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ ComicBookApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ComicBookApp.MvcApplication" Language="C#" %>
